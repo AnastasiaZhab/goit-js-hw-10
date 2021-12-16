6531ec7d8d48aab063c2fde2ce1859635d659c06
AnastasiaZhab <86264493+AnastasiaZhab@users.noreply.github.com>
2021-12-16 10:43:47 +0000

    Deploying to gh-pages from @ AnastasiaZhab/goit-js-hw-10@e381660dd356f4b78f66c352d947a0713dcd8d24 🚀

--- src.87657e9c.js
+++ src.87657e9c.js
@@ -1,6 +1,8 @@
 parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcelRequire,u="function"==typeof require&&require;function f(t,n){if(!r[t]){if(!e[t]){var i="function"==typeof parcelRequire&&parcelRequire;if(!n&&i)return i(t,!0);if(o)return o(t,!0);if(u&&"string"==typeof t)return u(t);var c=new Error("Cannot find module '"+t+"'");throw c.code="MODULE_NOT_FOUND",c}p.resolve=function(r){return e[t][1][r]||r},p.cache={};var l=r[t]=new f.Module(t);e[t][0].call(l.exports,p,l,l.exports,this)}return r[t].exports;function p(e){return f(p.resolve(e))}}f.isParcelRequire=!0,f.Module=function(e){this.id=e,this.bundle=f,this.exports={}},f.modules=e,f.cache=r,f.parent=o,f.register=function(r,t){e[r]=[function(e,r){r.exports=t},{}]};for(var c=0;c<t.length;c++)try{f(t[c])}catch(e){i||(i=e)}if(t.length){var l=f(t[t.length-1]);"object"==typeof exports&&"undefined"!=typeof module?module.exports=l:"function"==typeof define&&define.amd?define(function(){return l}):n&&(this[n]=l)}if(parcelRequire=f,i)throw i;return f}({"krre":[function(require,module,exports) {
 
+},{}],"SvwL":[function(require,module,exports) {
+"use strict";Object.defineProperty(exports,"__esModule",{value:!0}),exports.fetchCountries=void 0;const e=e=>{const t="https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages";console.log(t),fetch(t).then(e=>e.json()).then(console.log)};exports.fetchCountries=e;
 },{}],"Focm":[function(require,module,exports) {
-"use strict";require("./css/styles.css");const s=300;
-},{"./css/styles.css":"krre"}]},{},["Focm"], null)
-//# sourceMappingURL=/parcel-project-template/src.0dcbb10c.js.map
+"use strict";require("./css/styles.css");var e=require("./fetchCountries.js");console.log(e.fetchCountries);const t={inputEl:document.querySelector("#search-box"),countryListEl:document.querySelector(".country-list"),countryInfoEl:document.querySelector(".country-info")};function o(){let o=t.inputEl.value;console.log(o),(0,e.fetchCountries)(o)}t.inputEl.addEventListener("input",o);
+},{"./css/styles.css":"krre","./fetchCountries.js":"SvwL"}]},{},["Focm"], null)
+//# sourceMappingURL=/parcel-project-template/src.87657e9c.js.map
